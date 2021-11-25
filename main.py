03fdc58ccb1a7460e156137c4a8b2320527cebe8
Cole Goddin <cgoddin@zyst.co>
2021-11-24 20:41:30 -0500

    Added function for HTTP requests

--- main.py
+++ main.py
@@ -229,4 +229,14 @@ def main():
     
     set_with_dataframe(sheet, prospects)
 
-main()
+def gcp_func_entry(request):
+    
+    request_json = request.get_json()
+    if request.args and 'message' in request.args:
+        return request.args.get('message')
+    elif request_json and 'message' in request_json:
+        return request_json['message']
+    
+    main()
+    
+    return "Processing Completed!"
